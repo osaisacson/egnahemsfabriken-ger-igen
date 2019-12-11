ace591a77173ffa5b662b912d581698429fd8753
Osa Isacson <asaisacson@gmail.com>
2019-12-11 19:43:01 +0100

    test iOS and web changes

--- packages/components/src/AppHeader.tsx
+++ packages/components/src/AppHeader.tsx
@@ -1,5 +1,31 @@
-import {
-  Header as AppHeader
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Header as AppHeader
+// } from 'react-native/Libraries/NewAppScreen';
 
-export { AppHeader }
+// export { AppHeader }
+
+//This header is for web, iOS and android
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function AppHeader() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Egnahemsfabriken</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 200,
+    backgroundColor: '#f3f3f3'
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: '600'
+  }
+});
--- packages/components/src/AppHeader.web.tsx
+++ packages/components/src/AppHeader.web.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export function AppHeader() {
-  return <View style={styles.container}>
-      <Text style={styles.text}>Welcome to React Native Web + Monorepo</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Egnahemsfabriken</Text>
     </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -13,10 +15,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
     height: 200,
-    backgroundColor: '#f3f3f3',
+    backgroundColor: '#f3f3f3'
   },
   text: {
-   fontSize: 36,
-   fontWeight: '600',
+    fontSize: 36,
+    fontWeight: '600'
   }
-})
+});
